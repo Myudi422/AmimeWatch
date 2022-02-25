f6e178257aa89da9b828d79c784e17b5d20f0e0a
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-02-25 10:58:44 +0700

    Update and rename config.py.example to config.py

--- amime/config.py
+++ amime/config.py
@@ -15,5 +15,5 @@ CHATS: Dict = {
     "videos": 0,
     "staff": 0,
 }
-SUDO_USERS: List[int] = [1155717290, 918317361, 705707638]  # @AndrielFR, @Hitalo and @VegaZS
+SUDO_USERS: List[int] = [1765145725, 918317361, 705707638]  # @AndrielFR, @Hitalo and @VegaZS
 PREFIXES: List[str] = ["!", "/"] # Like !start or /start
