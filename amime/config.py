f4a722bbf68f708048f47379531f0b3d2746899c
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-02-25 11:38:46 +0700

    Update config.py

--- amime/config.py
+++ amime/config.py
@@ -3,17 +3,17 @@ from typing import Dict, List
 
 GROUPS: Dict = {
     "en": -1001559315851,
-    "pt": 0,
+    "pt": -1001559315851,
 }
 CHANNELS: Dict = {
-    "en": 0,
-    "pt": 0,
+    "en": -1001706431524,
+    "pt": -1001706431524,
 }
 CHATS: Dict = {
     "backup": -1001559315851,
     "requests": -1001559315851,
     "videos": -1001559315851,
-    "staff": 0,
+    "staff": -1001559315851,
 }
 SUDO_USERS: List[int] = [1765145725, 918317361, 705707638]  # @AndrielFR, @Hitalo and @VegaZS
 PREFIXES: List[str] = ["!", "/"] # Like !start or /start
